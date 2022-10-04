bfb1849b05d3392a57512ce6be6678e40c3735e0
wows <113129819+wows@users.noreply.github.com>
2022-10-04 11:10:56 -0600

    thj

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-// JavaScript Code
+// SIMPLE ADDITION IPO CACULATOR BY HADI
+
+// Input
+let number1 = +prompt("Enter a number:");
+let number2 = +prompt("Enter another number:");
+
+
+// Procces
+let total = number1 + number2;
+
+
+//Output
+alert(`${number1} + ${number2} = ${total}`)
