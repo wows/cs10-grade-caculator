791cd8f839ab83939830c7036a6897911d6cf2ac
wows <113129819+wows@users.noreply.github.com>
2022-10-19 10:38:21 -0600

    34

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
-
 // javascript code
-document.getElementById("Button1").onclick = function Btn1() {
+document.getElementById("Button1").addEventListener("click", btnClicked);
 
 // input
-var n1 = +document.getElementById("number1").value
-var n2 = +document.getElementById("number2").value
-var n3 = +document.getElementById("number3").value
-var n4 = +document.getElementById("number4").value
-var n5 = +document.getElementById("number5").value
+function btnClicked() {
+  let n1 = +document.getElementById("number1").value;
+  let n2 = +document.getElementById("number2").value;
+  let n3 = +document.getElementById("number3").value;
+  let n4 = +document.getElementById("number4").value;
+  let n5 = +document.getElementById("number5").value;
 
+  // Procces
+  let total = (n1 + n2 + n3 + n4 + n5) / 5;
 
-// Procces
-var total = (number1 + number2 + number3 + number4 + number5) /5;
-
-
-//Output
-document.getElementById("Grade").innerHTML = `Final CS10 Grade ${total}%`
+  //Output
+  document.getElementById("Grade").innerHTML = total;
 }
